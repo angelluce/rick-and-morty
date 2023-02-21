20046220af18e8fe7d66aaf7d279a868a6baa071
angellucero <angelluce244@gmail.com>
2023-02-20 20:22:43 -0500

    Add Social Networks service and model

--- src/app/services/rick-and-morty.service.ts
+++ src/app/services/rick-and-morty.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
@@ -7,12 +7,11 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 export class RickAndMortyService {
   headers = new HttpHeaders().append('Content-Type', 'application/json');
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {
+  }
 
   getData(url: string): Promise<any> {
-    return this._http.get<any>(
-      url
-    ).toPromise();
+    return this._http.get<any>(url).toPromise();
   }
 
 }
